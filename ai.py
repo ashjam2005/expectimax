8e265b90cfce492c7ea40a2624b61a08c388989c
ashjam2005 <122492177+ashjam2005@users.noreply.github.com>
2026-03-09 19:11:46 -0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -18,8 +18,9 @@ class Node:
 
     # returns whether this is a terminal state (i.e., no children)
     def is_terminal(self):
-        #TODO: complete this
-        pass
+        if (len(self.children) == 0):
+            return True
+
 
 # AI agent. Determine the next move.
 class AI:
@@ -34,14 +35,113 @@ class AI:
 
     # TODO: build a game tree from the current node up to the given depth
     def build_tree(self, node = None, depth = 0):
-        pass
+        if (depth == 0):
+            return
+
+        # Checking if we at MAX_PLAYER
+        if (node.player_type == MAX_PLAYER):
+
+            self.simulator.set_state(*node.state)            
+
+            state_right_now = self.simulator.current_state()
+            
+            for direction in MOVES:
+                
+                validMove =  self.simulator.move(direction)
+                
+                if (state_right_now == self.simulator.current_state() and validMove):
+                    print("What the hell bruh!")
+
+                #Checking if you can do the move
+                if (validMove):
+
+                    #Resulting state due to moving in certain valid direction
+
+                    amended_state = self.simulator.current_state()
+
+                    #Create child Node and inserting it into children list
+                    
+                    child = Node(amended_state,CHANCE_PLAYER)
+                    node.children.append((direction,child))
+                    self.build_tree(child,depth-1)
+                
+                self.simulator.set_state(*state_right_now)
+             
+            
+            
+        
+
+        elif (node.player_type == CHANCE_PLAYER):
+             
+             
+             
+             #Get unoccupied tiles
+
+             self.simulator.set_state(*node.state)
+             
+
+             state_right_now = self.simulator.current_state()
+             unoccupied_tiles = self.simulator.get_open_tiles()
+
+             tile_matrix = state_right_now[0]
+             score = state_right_now[1]
+
+             
+
+             #Iterate over unoccupied tiles
+             for i,j in unoccupied_tiles:
+                                               
+                amended_tile = copy.deepcopy(tile_matrix)              
+                amended_tile[i][j] = 2 
+
+                
+                             
+                new_state = (amended_tile, score)                
+                child = Node(new_state, MAX_PLAYER)                
+                node.children.append((None,child))               
+                self.build_tree(child, depth - 1)
+
+
 
     # TODO: expectimax calculation.
     # Return a (best direction, expectimax value) tuple if node is a MAX_PLAYER
     # Return a (None, expectimax value) tuple if node is a CHANCE_PLAYER
     def expectimax(self, node = None):
         # TODO: delete this random choice but make sure the return type of the function is the same
-        return random.randint(0, 3), 0
+        if (node.is_terminal() == True): #node[1].is_terminal
+            return (None,node.state[1])
+
+
+        elif(node.player_type == MAX_PLAYER):    #node[1].is_terminal
+            
+            
+            max = 0
+            max_direction = None
+
+            for (direction,child) in node.children:    #node[1].children
+               
+                #n, value = self.expectimax((direction,child))
+                n, value = self.expectimax(child)
+                if (value > max):
+                    max = value
+                    max_direction = direction
+            
+
+            return (max_direction,max)
+
+        elif(node.player_type == CHANCE_PLAYER):   #node[1]
+
+            sum = 0
+            
+            for (direction,child) in node.children:   #node[1]
+                
+                _, value = self.expectimax(child)             
+                sum += value
+                            
+            average = sum/len(node.children)     #node[1]
+            return (None,average)
+
+    
 
     # Return decision at the root
     def compute_decision(self):
